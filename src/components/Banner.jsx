3106c692a87751312d0e9275e10433376136c30b
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 16:49:45 +0100

    add to flex

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,12 +12,12 @@ const Banner = () => {
         <h1 className='text-4x1 lg:text-[58px] font-semibold leading-none mb-6'>
           <span className='text-violet-700'>Rent</span>Your Dream House with Us.
         </h1>
-        <p>
+        <p className='max-w-[480px] mb-8'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet rerum alias dolorem quos quae illo repudiandae pariatur nostrum accusantium enim ipsam, quisquam ab minus eius animi consectetur aspernatur accusamus! Beatae.
         </p>
       </div>
       {/* image */}
-      <div>
+      <div className='hidden flex-1 lg:flex justify-end items-end'>
         <img src={Image} alt="" />
       </div>
     </div>
