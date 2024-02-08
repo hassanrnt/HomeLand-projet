033e8158dd440cddffe399fcc89c147ac32101ab
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 16:45:31 +0100

    add to color

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,10 +6,11 @@ import Image from '../assets/img/house-banner.png';
 import Search from '../components/Search';
 
 const Banner = () => {
-  return <section>
-    <div>
-      <div>
-        <h1><span>Rent</span>Your Dream House with Us.
+  return <section className='h-full max-h-[640px] mb-8 x1:mb-24'>
+    <div className='flex flex-col lg:flex-row'>
+      <div className='lg:ml-8 x1:ml-[135] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
+        <h1 className='text-4x1 lg:text-[58px] font-semibold leading-none mb-6'>
+          <span className='text-violet-700'>Rent</span>Your Dream House with Us.
         </h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet rerum alias dolorem quos quae illo repudiandae pariatur nostrum accusantium enim ipsam, quisquam ab minus eius animi consectetur aspernatur accusamus! Beatae.
