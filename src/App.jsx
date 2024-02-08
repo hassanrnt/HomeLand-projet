9c6a1e6cac329e7ab0ad4dca0cb6874301661e2a
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 14:59:49 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import routes and route
+import {Routes, Route} from 'react-router-dom';
+
+
+// import components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return <div>
+    <Header/>
+    <Footer/>
+  </div>;
+};
+
+export default App;
