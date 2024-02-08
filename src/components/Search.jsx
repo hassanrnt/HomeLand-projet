d0cfdf4e771fd979d8d7d4a64deaae332f3be25a
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 17:17:28 +0100

    add to search Search2Line

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
-// import components 
-import CountryDropdown from './CountryDropdown'
-import PropertyDropdown from './PropertyDropdown'
-import PriceRangeDropdown from './PriceRangeDropdown'
+// import components
+import CountryDropdown from './CountryDropdown';
+import PropertyDropdown from './PropertyDropdown';
+import PriceRangeDropdown from './PriceRangeDropdown';
 
+
+// import icons
+import {RiSearch2Line} from 'react-icons/ri';
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+    <CountryDropdown/>
+    <PropertyDropdown/>
+    <PriceRangeDropdown/>
+    <button>
+      <RiSearch2Line/>
+    </button>
+  </div>
+  );
 };
 
 export default Search;
