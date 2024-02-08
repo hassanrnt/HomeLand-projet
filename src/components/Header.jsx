008df99d8d7f0899dccb45c09c8ad52b7f88fb9d
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 15:45:26 +0100

    add to sign in and sign up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,17 @@ import {Link} from'react-router-dom';
 // import logo
 import Logo from'../assets/img/logo.svg';
 const Header = () => {
-  return <header>
-    <div className="container mx-auto">
+  return <header className='py-6 mb-12 border-b'>
+    <div className="container mx-auto flex">
       {/* logo */}
       <Link to='/'>
         <img src={Logo} alt=''/>
       </Link>
+      {/* buttons */}
+      <div>
+        <Link to=''>Sign in</Link>
+        <Link to=''>Sign up</Link>
+      </div>
     </div>
   </header>;
 };
