41ff54f72a9c1e9595b9813e454a7c54ef8b9d7f
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 16:54:47 +0100

    add to search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// import components 
+import CountryDropdown from './CountryDropdown'
+import PropertyDropdown from './PropertyDropdown'
+import PriceRangeDropdown from './PriceRangeDropdown'
+
 const Search = () => {
   return <div>Search</div>;
 };
