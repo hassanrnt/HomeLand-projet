351baf1571646bc3cf35771474b057d50ebe8443
hassanrnt <hassanritouny97@gmail.com>
2024-02-08 15:20:48 +0100

    add to component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+// import Link
+import {Link} from'react-router-dom';
+// import logo
+import Logo from'../assets/img/loho.svg';
 const Header = () => {
-  return <div>Header</div>;
+  return <header>
+    <div className="container mx-auto">
+      {/* logo */}
+      <Link></Link>
+    </div>
+  </header>;
 };
 
 export default Header;
