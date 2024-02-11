48fe92bcd8cd36431c4fc86774605143097defdd
hassanrnt <hassanritouny97@gmail.com>
2024-02-11 17:13:45 +0100

    add to headlessui in component CountryDrown

--- src/components/CountryDropdown.jsx
+++ src/components/CountryDropdown.jsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+// import icons
+import {RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine} from 'react-icons/ri'
+// import headless ul
+import {Menu} from '@headlessui/react'
+
+// import house context
+import {} from './HouseContext';
 
 const CountryDropdown = () => {
   return <div>CountryDropdown</div>;
