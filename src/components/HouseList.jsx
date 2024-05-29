c01f15ad35e98055d9e705a760a2c8662e8e6602
hassanrnt <hassanritouny97@gmail.com>
2024-05-29 21:25:01 +0200

    add to HouseList

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -16,7 +16,19 @@ const HouseList = () => {
   const {houses, loading} = useContext(HouseContext);
   console.log(houses)
 
-  return <div>HouseList</div>;
+  return <section className='mb-20'>
+    <div className='container mx-auto'>
+      <div>
+        {houses.map((house, index)=> {
+          return(
+            <Link to={`property/$(house.id)`} key={index}>
+              <House house={house}/>
+            </Link>
+          )
+        })}
+      </div>
+    </div>
+  </section>;
 };
 
 export default HouseList;
