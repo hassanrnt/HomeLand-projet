94db752d6669b5e04581846d386e837d6683da14
hassanrnt <hassanritouny97@gmail.com>
2024-05-29 14:12:34 +0200

    add to houseContext

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -1,4 +1,17 @@
-import React from 'react';
+import React , {useContext}from 'react';
+
+// import context
+import {HouseContext} from './HouseContext';
+
+// import components 
+import House from './House';
+
+// import Link
+import {Link} from 'react-router-dom';
+// import icons
+import {ImSpinner2} from 'react-icons/im';
+const {Houses, loading} = useContext(HouseContext);
+console.log(Houses)
 
 const HouseList = () => {
   return <div>HouseList</div>;
