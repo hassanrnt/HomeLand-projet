023253cdf6ccd9e47f05e89dde3890294415b8e4
hassanrnt <hassanritouny97@gmail.com>
2024-05-30 11:08:00 +0200

    add to type and Country

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+
 // import icons
-import {BiBed, BiBath, BiArea} from 'react-icons/bi'
+import {BiBed, BiBath, BiArea } from 'react-icons/bi';
+
 
 const House = ({house}) => {
-  const {image, type, country, addres, bedrooms, bathroms, surface, price} = house;
-  return <div>
-    <img src={image} alt="" />
-  </div>;
+  const {image, type, country, address, bedrooms, bathrooms,surface, price} = house;
+  return (
+    <div className='bg-white shadow-1 p-5 rounded-lg rounded-t1-[90PX] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2x1 transition'>
+    <img className='mb-8' src={image} alt="" />
+    <div className='mb-4 flex gap-x-2 text-sm'>
+      <div className='bg-green-500 rounded-full text-white px-3'>{type}</div>
+      <div>{country}</div>
+    </div>
+  </div>
+  );
 };
 
 export default House;
