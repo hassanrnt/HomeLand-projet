737a1eb3717ebe19ae42b0687cccdb634ca0fd86
hassanrnt <hassanritouny97@gmail.com>
2024-05-30 11:37:56 +0200

    add to adress price surface

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -12,8 +12,28 @@ const House = ({house}) => {
     <img className='mb-8' src={image} alt="" />
     <div className='mb-4 flex gap-x-2 text-sm'>
       <div className='bg-green-500 rounded-full text-white px-3'>{type}</div>
-      <div>{country}</div>
+      <div className='bg-violet-500 rounded-full text-white px-3'>{country}</div>
     </div>
+    <div className='text-lg font-semibold max-w-[260px]'>{address}</div>
+    <div className='flex items-center text-gray-600 gap-1'>
+      <div className='text-[20px]'>
+        <BiBed/>
+      </div>
+      <div>{bedrooms}</div>
+    </div>
+    <div className='flex items-center text-gray-600 gap-1'>
+      <div className='text-[20px]'>
+        <BiBath/>
+      </div>
+      <div>{bathrooms}</div>
+    </div>
+    <div className='flex items-center text-gray-600 gap-1'>
+      <div className='text-[20px]'>
+        <BiArea/>
+      </div>
+      <div>{surface}</div>
+    </div>
+    <div className='text-lg font-semibold text-violet-600 mb-4'>{price}</div>
   </div>
   );
 };
