e500b1c4eb834d737d61537603cef59cf31789fb
magsi212 <zeenenglish@gmail.com>
2024-06-01 21:59:59 +0100

    edit the code

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 // import data 
-import { houseData } from '../data';
+import { housesData } from '../data';
 
 // import use params
 import {useParams} from 'react-router-dom';
 
 // import icons
-import {BiBed, BiBath, Biarea} from 'react-icons/bi';
+import {BiBed, BiBath, BiArea} from 'react-icons/bi';
 
 // import link
-import {} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const PropertyDetails = () => {
