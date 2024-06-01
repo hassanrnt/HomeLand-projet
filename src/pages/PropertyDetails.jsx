a4664ddaf6f77b88076bf5d824a6ab622f96a94b
hassanrnt <hassanritouny97@gmail.com>
2024-06-01 21:37:53 +0200

    add to id

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
+// import data 
+import { houseData } from '../data';
+
+// import use params
+import {useParams} from 'react-router-dom';
+
+// import icons
+import {BiBed, BiBath, Biarea} from 'react-icons/bi';
+
+// import link
+import {} from 'react-router-dom';
+
+
 const PropertyDetails = () => {
+
+  // get the house id
+  const {id} = useParams ();
+  console.log(id);
   return <div>PropertyDetails</div>;
 };
 
