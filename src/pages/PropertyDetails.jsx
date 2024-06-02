812586c6a2f6cd8e2ae76498251c7351ff2c7374
hassanrnt <hassanritouny97@gmail.com>
2024-06-02 16:14:12 +0200

    add to information d'un house and aparttement

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -17,8 +17,28 @@ const PropertyDetails = () => {
 
   // get the house id
   const {id} = useParams ();
-  console.log(id);
-  return <div>PropertyDetails</div>;
+   // get the house based on the id 
+
+   const house = housesData.find(house => {
+    return house.id === parseInt(id);
+   });
+
+   
+  return <section>
+    <div className="container mx-auto min-h-[800px] mb-14">
+      <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between'>
+        <div>
+          <h2 className='text-2x1 font-semibold'>{house.name}</h2>
+          <h3 className='text-lg mb-4'>{house.address}</h3>
+        </div>
+        <div className='mb-4 lg:mb-0 flex  gap-x-2 text-sm'>
+          <div className='bg-green-500 text-white px-3 rounded-full'>{house.type}</div>
+          <div className='bg-violet-500 text-white px-3 rounded-full'>{house.country}</div>
+        </div>
+        <div className='text 3x1 font-semibold text-violet-600'> MAD {house.price}</div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default PropertyDetails;
