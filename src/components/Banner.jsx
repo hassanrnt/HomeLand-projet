009144edfd9bc51b3ce19808c9b5e9ee13c1598b
hassanrnt <hassanritouny97@gmail.com>
2024-06-05 01:26:12 +0200

    modification final

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,10 +10,10 @@ const Banner = () => {
     <div className='flex flex-col lg:flex-row'>
       <div className='lg:ml-8 x1:ml-[135] flex flex-col items-center lg:items-start text-center lg:text-left justify-center flex-1 px-4 lg:px-0'>
         <h1 className='text-4x1 lg:text-[40px] font-semibold leading-none mb-6'>
-          <span className='text-violet-700'>J'achète</span> votre maison de rêve<br/> avec nous.
+          <span className='text-violet-700'>Achetez</span> votre maison de rêve <br/> avec nous.
         </h1>
         <p className='max-w-[480px] mb-8'>
-        HomeLand est bien plus qu'une agence immobilière. Nos amis professionnels de l'immobilier sont rigoureusement sélectionnés à travers tout le Maroc pour leurs compétences professionnelles et leurs valeurs humaines. Plus que des agents, nos AMIS s'engagent à faire de votre projet immobilier une expérience exceptionnelle.
+        Bienvenue chez HomeLand, bien plus qu'une agence immobilière. Nos professionnels, rigoureusement sélectionnés à travers tout le Maroc pour leurs compétences et leurs valeurs humaines, s'engagent à faire de votre projet immobilier une expérience exceptionnelle. Que vous cherchiez à acheter,  nos AMIS sont là pour vous accompagner à chaque étape avec professionnalisme et bienveillance.
         </p>
       </div>
       {/* image */}
